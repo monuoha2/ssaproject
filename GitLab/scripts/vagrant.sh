d8371fe51671de0d65ba62730770adc3a734490b
rrobin77 <roy.s.robinson.rr@gmail.com>
2019-01-24 02:08:44 -0500

    Added virturalBox install instructions for CentOS7

---
+++ GitLab/scripts/vagrant.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo " Installing Vagrant from https://releases.hashicorp.com/vagrant/2.2.3/vagrant_2.2.3_x86_64.rpm !"
+
+yum install -y https://releases.hashicorp.com/vagrant/2.2.3/vagrant_2.2.3_x86_64.rpm
